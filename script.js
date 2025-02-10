486f45cab80802edf2131c6715521fb3372c038b
Suhaani Garg <suhaanigarg2311@gmail.com>
2025-02-10 19:21:00 +0530

    Seventh Commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const display = document.getElementById("display");
 
-function appendToDisplay(input){
+function appendToDisplay(input) {
     display.value += input;
 }
 
-function clearDisplay(){
+function clearDisplay() {
     display.value = "";
 }
 
-function calculate(){
-    try{
-        display.value = eval(display.value);
-    }
-    catch(error){
+function calculate() {
+    try {
+        let result = new Function(`return ${display.value}`)();  
+        display.value = result;
+    } catch (error) {
         display.value = "Error";
     }
 }
